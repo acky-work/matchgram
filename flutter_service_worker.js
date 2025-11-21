972a68af929b1ac8d301c576337f84e4084bfed0
Akira Nishikawa <akira@ugotoru.com>
2025-11-21 10:46:00 +0900

    fix: base hrefの修正

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e403e67f1ca9df43c7b391494c430067",
+const RESOURCES = {"flutter_bootstrap.js": "1e88f1c61686a0d4bac656c58333bf6c",
 "version.json": "11a82aaa55fd6b1d66da9b6b28f8a954",
-"index.html": "4a2a47a0f5e1eb2596368214109b7f5e",
-"/": "4a2a47a0f5e1eb2596368214109b7f5e",
+"index.html": "2d216196a9bd4cc9c10eb701688074d3",
+"/": "2d216196a9bd4cc9c10eb701688074d3",
 "main.dart.js": "0a840a52b8b73ff99b419d2d4c572126",
 "404.html": "2d216196a9bd4cc9c10eb701688074d3",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
